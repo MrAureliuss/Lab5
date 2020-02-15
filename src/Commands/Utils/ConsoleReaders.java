7d980a13831e66b2fd593397d734e786eb9dbec7
avelok <mraurelius@yandex.ru>
2020-02-15 16:30:23 +0300

    Initial Commit

--- src/Commands/Utils/ConsoleReaders.java
+++ src/Commands/Utils/ConsoleReaders.java
@@ -10,11 +10,10 @@ public class ConsoleReaders {
         Scanner in = new Scanner(System.in);
         System.out.print(messageForConsole);
         String readString = in.nextLine().trim();
-        if(!canBeNull && readString.equals("")) {
-            while (readString.equals("")) {
-                System.out.print("Данное поле не может быть пустым. " + messageForConsole);
-                readString = in.nextLine();
-            }
+
+        while(!canBeNull && readString.equals("")) {
+            System.out.print("Данное поле не может быть пустым. " + messageForConsole);
+            readString = in.nextLine().trim();
         }
 
         if(canBeNull && readString.equals("")) { readString = null; }
@@ -25,11 +24,10 @@ public class ConsoleReaders {
         Scanner in = new Scanner(System.in);
         System.out.print(messageForConsole);
         Integer readInt = in.nextInt();
-        if(!canBeNull && readInt.toString().equals("")) {
-            while (readInt.toString().equals("")) {
-                System.out.print("Данное поле не может быть пустым. " + messageForConsole);
-                readInt = in.nextInt();
-            }
+
+        while(!canBeNull && readInt.toString().equals("")) {
+            System.out.print("Данное поле не может быть пустым. " + messageForConsole);
+            readInt = in.nextInt();
         }
 
         if(canBeNull && readInt.toString().equals("")) { readInt = null; }
@@ -40,11 +38,10 @@ public class ConsoleReaders {
         Scanner in = new Scanner(System.in);
         System.out.print(messageForConsole);
         float readFloat = in.nextFloat();
-        if(String.valueOf(readFloat).equals("")) {
-            while (String.valueOf(readFloat).equals("")) {
-                System.out.print("Данное поле не может быть пустым. " + messageForConsole);
-                readFloat = in.nextInt();
-            }
+
+        while(String.valueOf(readFloat).equals("")) {
+            System.out.print("Данное поле не может быть пустым. " + messageForConsole);
+            readFloat = in.nextInt();
         }
 
         return readFloat;
@@ -55,5 +52,12 @@ public class ConsoleReaders {
         System.out.print("Введите страну из представленных(" + Arrays.asList(Country.values()) + "): ");
         String toContains = in.nextLine().trim();
 
+        while (Arrays.stream(Country.values()).noneMatch((country) -> country.name().equals(toContains))) {
+            System.out.print("Вы ввели несуществующее значение из представленных. Попробуйте снова: ");
+            toContains = in.nextLine().trim();
+            
+        }
+
+        return Enum.valueOf(Country.class, toContains);
     }
 }
